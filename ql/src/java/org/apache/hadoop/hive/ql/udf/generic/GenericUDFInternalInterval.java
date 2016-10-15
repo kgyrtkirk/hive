92e6f0d032f9a0e628529f5fead65d1ea26f651a
Haindrich Zoltán (kirk) <kirk@rxd.hu>
2016-10-15 20:22:57 +0200

    add all the others too

--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFInternalInterval.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFInternalInterval.java
@@ -32,6 +32,7 @@ import org.apache.hadoop.hive.ql.parse.HiveParser;
 import org.apache.hadoop.hive.ql.plan.ExprNodeConstantDesc;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDFUtils.ReturnObjectInspectorResolver;
 import org.apache.hadoop.hive.serde2.io.HiveIntervalDayTimeWritable;
+import org.apache.hadoop.hive.serde2.io.HiveIntervalYearMonthWritable;
 import org.apache.hadoop.hive.serde2.objectinspector.ConstantObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
@@ -42,34 +43,30 @@ import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectIn
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils.PrimitiveGrouping;
 import org.apache.hadoop.hive.serde2.typeinfo.PrimitiveTypeInfo;
 import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoFactory;
+import org.apache.hive.common.util.DateUtils;
 
 /**
  * GenericUDF Class for support of "INTERVAL (expression) (DAY|YEAR|...)".
  */
-@Description(
-    name = "internal_interval",
-    value = "_FUNC_(a1,a2) - later",
-    extended = "Example:\n "
-        + "probably later I may write something here...")
-
+@Description(name = "internal_interval", value = "_FUNC_(a1,a2) - later", extended = "Example:\n "
+    + "probably later I may write something here...")
 
 public class GenericUDFInternalInterval extends GenericUDF {
   private transient ObjectInspector[] argumentOIs;
   private transient GenericUDFUtils.ReturnObjectInspectorResolver returnOIResolver;
-  private AbstractPrimitiveWritableObjectInspector resultOI;
-
+  private transient AbstractPrimitiveWritableObjectInspector resultOI;
 
   protected transient HiveIntervalDayTimeWritable intervalDayTimeResult =
       new HiveIntervalDayTimeWritable();
-  private Integer operationMode;
-  private IntervalProcessor processor;
-  private PrimitiveObjectInspector inputOI;
-  
+  private transient Integer operationMode;
+  private transient IntervalProcessor processor;
+  private transient PrimitiveObjectInspector inputOI;
+
   @Override
   public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
-    
+
     argumentOIs = arguments;
-    
+
     // read operation mode
     if (!(arguments[1] instanceof ConstantObjectInspector)) {
       throw new UDFArgumentTypeException(1,
@@ -79,160 +76,268 @@ public class GenericUDFInternalInterval extends GenericUDF {
     if (operationMode == null) {
       throw new UDFArgumentTypeException(1, "must supply operationmode");
     }
-    
+
     processor = getProcessorMap().get(operationMode);
-    if(processor==null){
+    if (processor == null) {
       throw new UDFArgumentTypeException(1,
-          getFuncName() + ": unsupported operationMode: "+operationMode);
+          getFuncName() + ": unsupported operationMode: " + operationMode);
     }
-    
+
     // FIXME: check arg0
     if (arguments[0].getCategory() != Category.PRIMITIVE) {
-      throw new UDFArgumentTypeException(0, "The first argument to "+getFuncName()+" must be primitive");
+      throw new UDFArgumentTypeException(0,
+          "The first argument to " + getFuncName() + " must be primitive");
     }
 
     inputOI = (PrimitiveObjectInspector) arguments[0];
 
-//    if (PrimitiveGrouping.STRING_GROUP != PrimitiveObjectInspectorUtils
-//        .getPrimitiveGrouping(inputOI.getPrimitiveCategory())) {
-//      throw new UDFArgumentTypeException(0,
-//          "The first argument to "+getFuncName()+" must be fromstring group");
-//    }
-    
+    // if (PrimitiveGrouping.STRING_GROUP != PrimitiveObjectInspectorUtils
+    // .getPrimitiveGrouping(inputOI.getPrimitiveCategory())) {
+    // throw new UDFArgumentTypeException(0,
+    // "The first argument to "+getFuncName()+" must be fromstring group");
+    // }
+
     resultOI = PrimitiveObjectInspectorFactory
         .getPrimitiveWritableObjectInspector(processor.getTypeInfo());
-    
+
     return resultOI;
   }
 
-  
   @Override
   public Object evaluate(DeferredObject[] arguments) throws HiveException {
-    String argString=PrimitiveObjectInspectorUtils.getString(arguments[0].get(),inputOI);
+    String argString = PrimitiveObjectInspectorUtils.getString(arguments[0].get(), inputOI);
     return processor.evaluate(argString);
-//    stringResolver.convertIfNecessary(arguments[0], TypeInfoFactory.stringTypeInfo, false);
-//
-//    if (PrimitiveObjectInspectorUtils.comparePrimitiveObjects(
-//        arg0, compareOI,
-//        ) {
-//      return null;
-//    }
-//
-////    Object arg0 = arguments[0].get();
-////    Object arg1 = arguments[1].get();
-////    if (arg0 == null || arg1 == null) {
-////      return arg0;
-////    }
-////    PrimitiveObjectInspector compareOI = (PrimitiveObjectInspector) returnOIResolver.get();
-////    if (PrimitiveObjectInspectorUtils.comparePrimitiveObjects(
-////        arg0, compareOI,
-////        returnOIResolver.convertIfNecessary(arg1, argumentOIs[1], false), compareOI)) {
-////      return null;
-////    }
-//    HiveIntervalDayTime hidt = new HiveIntervalDayTime(100000,0);
-//    intervalDayTimeResult.set(hidt);
-//    return intervalDayTimeResult;
+    // stringResolver.convertIfNecessary(arguments[0], TypeInfoFactory.stringTypeInfo, false);
+    //
+    // if (PrimitiveObjectInspectorUtils.comparePrimitiveObjects(
+    // arg0, compareOI,
+    // ) {
+    // return null;
+    // }
+    //
+    //// Object arg0 = arguments[0].get();
+    //// Object arg1 = arguments[1].get();
+    //// if (arg0 == null || arg1 == null) {
+    //// return arg0;
+    //// }
+    //// PrimitiveObjectInspector compareOI = (PrimitiveObjectInspector) returnOIResolver.get();
+    //// if (PrimitiveObjectInspectorUtils.comparePrimitiveObjects(
+    //// arg0, compareOI,
+    //// returnOIResolver.convertIfNecessary(arg1, argumentOIs[1], false), compareOI)) {
+    //// return null;
+    //// }
+    // HiveIntervalDayTime hidt = new HiveIntervalDayTime(100000,0);
+    // intervalDayTimeResult.set(hidt);
+    // return intervalDayTimeResult;
   }
 
-  
-  
   private static interface IntervalProcessor {
 
     Integer getKey();
+
     PrimitiveTypeInfo getTypeInfo();
-    Object evaluate(String arg);
+
+    Object evaluate(String arg) throws UDFArgumentException;
   }
 
-  private static class IntervalDayLiteralProcessor implements IntervalProcessor {
-    protected transient HiveIntervalDayTimeWritable intervalDayTimeResult =
+  private static abstract class AbstractDayTimeIntervalProcessor implements IntervalProcessor {
+    private transient HiveIntervalDayTimeWritable intervalResult =
         new HiveIntervalDayTimeWritable();
-    
+
+    @Override
+    public final PrimitiveTypeInfo getTypeInfo() {
+      return TypeInfoFactory.intervalDayTimeTypeInfo;
+    }
+
+    @Override
+    public final Object evaluate(String arg) throws UDFArgumentException {
+      intervalResult.set(getIntervalDayTime(arg));
+      return intervalResult;
+    }
+
+    abstract protected HiveIntervalDayTime getIntervalDayTime(String arg);
+  }
+
+  private static abstract class AbstractYearMonthIntervalProcessor implements IntervalProcessor {
+    private transient HiveIntervalYearMonthWritable intervalResult =
+        new HiveIntervalYearMonthWritable();
+
+    @Override
+    public final PrimitiveTypeInfo getTypeInfo() {
+      return TypeInfoFactory.intervalYearMonthTypeInfo;
+    }
+
+    @Override
+    public final Object evaluate(String arg) throws UDFArgumentException {
+      intervalResult.set(getIntervalYearMonth(arg));
+      return intervalResult;
+    }
+
+    abstract protected HiveIntervalYearMonth getIntervalYearMonth(String arg);
+  }
+  
+  private static class IntervalDayLiteralProcessor extends AbstractDayTimeIntervalProcessor {
+
     @Override
     public Integer getKey() {
       return HiveParser.TOK_INTERVAL_DAY_LITERAL;
     }
 
     @Override
-    public PrimitiveTypeInfo getTypeInfo() {
-      return TypeInfoFactory.intervalDayTimeTypeInfo;
-    }
-
-    @Override
-    public Object evaluate(String arg) {
-      HiveIntervalDayTime time = new HiveIntervalDayTime(Integer.parseInt(arg), 0, 0, 0, 0);
-      intervalDayTimeResult.set(time);
-      return intervalDayTimeResult;
+    protected HiveIntervalDayTime getIntervalDayTime(String arg) {
+      return new HiveIntervalDayTime(Integer.parseInt(arg), 0, 0, 0, 0);
     }
   }
-  private static class IntervalHourLiteralProcessor implements IntervalProcessor {
+
+  private static class IntervalHourLiteralProcessor extends AbstractDayTimeIntervalProcessor {
     @Override
     public Integer getKey() {
       return HiveParser.TOK_INTERVAL_HOUR_LITERAL;
     }
 
     @Override
-    public PrimitiveTypeInfo getTypeInfo() {
-      return TypeInfoFactory.intervalDayTimeTypeInfo;
+    protected HiveIntervalDayTime getIntervalDayTime(String arg) {
+      return new HiveIntervalDayTime(0, Integer.parseInt(arg), 0, 0, 0);
     }
-    protected transient HiveIntervalDayTimeWritable intervalDayTimeResult =
-        new HiveIntervalDayTimeWritable();
+  }
+  private static class IntervalMinuteLiteralProcessor extends AbstractDayTimeIntervalProcessor {
+    @Override
+    public Integer getKey() {
+      return HiveParser.TOK_INTERVAL_MINUTE_LITERAL;
+    }
 
     @Override
-    public Object evaluate(String arg) {
-      HiveIntervalDayTime time = new HiveIntervalDayTime(0,Integer.parseInt(arg), 0, 0, 0);
-      intervalDayTimeResult.set(time);
-      return intervalDayTimeResult;
+    protected HiveIntervalDayTime getIntervalDayTime(String arg) {
+      return new HiveIntervalDayTime(0, 0, Integer.parseInt(arg), 0, 0);
+    }
+  }
+  private static class IntervalSecondLiteralProcessor extends AbstractDayTimeIntervalProcessor {
+    
+    private static final BigDecimal NANOS_PER_SEC_BD = new BigDecimal(DateUtils.NANOS_PER_SEC);
+    
+    @Override
+    public Integer getKey() {
+      return HiveParser.TOK_INTERVAL_SECOND_LITERAL;
+    }
+
+    @Override
+    protected HiveIntervalDayTime getIntervalDayTime(String arg) {
+       BigDecimal bd = new BigDecimal(arg);
+       BigDecimal bdSeconds = new BigDecimal(bd.toBigInteger());
+       BigDecimal bdNanos = bd.subtract(bdSeconds);
+       new HiveIntervalDayTime(0, 0, 0, bdSeconds.intValueExact(),
+       bdNanos.multiply(NANOS_PER_SEC_BD).intValue());
+
+      return new HiveIntervalDayTime(0, 0, Integer.parseInt(arg), 0, 0);
+    }
+  }
+
+  private static class IntervalDayTimeLiteralProcessor extends AbstractDayTimeIntervalProcessor {
+    
+    @Override
+    public Integer getKey() {
+      return HiveParser.TOK_INTERVAL_DAY_TIME_LITERAL;
+    }
+
+    @Override
+    protected HiveIntervalDayTime getIntervalDayTime(String arg) {
+      return HiveIntervalDayTime.valueOf(arg);
     }
   }
   
-  private Map<Integer,IntervalProcessor> getProcessorMap() {
-    Map<Integer, IntervalProcessor> ret=new HashMap<>();
-    {
-      IntervalDayLiteralProcessor p = new IntervalDayLiteralProcessor();
-      ret.put(p.getKey(), p);
-    }
-//  case HiveParser.TOK_INTERVAL_HOUR_LITERAL:
-//  return new ExprNodeConstantDesc(TypeInfoFactory.intervalDayTimeTypeInfo,
-//      new HiveIntervalDayTime(0, Integer.parseInt(intervalString), 0, 0, 0));
-    {
-      IntervalHourLiteralProcessor p = new IntervalHourLiteralProcessor();
-      ret.put(p.getKey(), p);
-    }
+  
+  private static class IntervalYearMonthLiteralProcessor extends AbstractYearMonthIntervalProcessor {
     
+    @Override
+    public Integer getKey() {
+      return HiveParser.TOK_INTERVAL_YEAR_MONTH_LITERAL;
+    }
+
+    @Override
+    protected HiveIntervalYearMonth getIntervalYearMonth(String arg) {
+      return HiveIntervalYearMonth.valueOf(arg);
+    }
+  }
+  private static class IntervalYearLiteralProcessor extends AbstractYearMonthIntervalProcessor {
     
-//    switch (expr.getType()) {
-//    case HiveParser.TOK_INTERVAL_YEAR_MONTH_LITERAL:
-//      return new ExprNodeConstantDesc(TypeInfoFactory.intervalYearMonthTypeInfo,
-//          HiveIntervalYearMonth.valueOf(intervalString));
-//    case HiveParser.TOK_INTERVAL_DAY_TIME_LITERAL:
-//      return new ExprNodeConstantDesc(TypeInfoFactory.intervalDayTimeTypeInfo,
-//          HiveIntervalDayTime.valueOf(intervalString));
-//    case HiveParser.TOK_INTERVAL_YEAR_LITERAL:
-//      return new ExprNodeConstantDesc(TypeInfoFactory.intervalYearMonthTypeInfo,
-//          new HiveIntervalYearMonth(Integer.parseInt(intervalString), 0));
-//    case HiveParser.TOK_INTERVAL_MONTH_LITERAL:
-//      return new ExprNodeConstantDesc(TypeInfoFactory.intervalYearMonthTypeInfo,
-//          new HiveIntervalYearMonth(0, Integer.parseInt(intervalString)));
-//    case HiveParser.TOK_INTERVAL_DAY_LITERAL:
-//      return new ExprNodeConstantDesc(TypeInfoFactory.intervalDayTimeTypeInfo,
-//          // FIXME HIVE-13557 remove this hoax
-//          new HiveIntervalDayTime(Integer.parseInt("42"), 0, 0, 0, 0));
-//    case HiveParser.TOK_INTERVAL_HOUR_LITERAL:
-//      return new ExprNodeConstantDesc(TypeInfoFactory.intervalDayTimeTypeInfo,
-//          new HiveIntervalDayTime(0, Integer.parseInt(intervalString), 0, 0, 0));
-//    case HiveParser.TOK_INTERVAL_MINUTE_LITERAL:
-//      return new ExprNodeConstantDesc(TypeInfoFactory.intervalDayTimeTypeInfo,
-//          new HiveIntervalDayTime(0, 0, Integer.parseInt(intervalString), 0, 0));
-//    case HiveParser.TOK_INTERVAL_SECOND_LITERAL:
-//      BigDecimal bd = new BigDecimal(intervalString);
-//      BigDecimal bdSeconds = new BigDecimal(bd.toBigInteger());
-//      BigDecimal bdNanos = bd.subtract(bdSeconds);
-//      return new ExprNodeConstantDesc(TypeInfoFactory.intervalDayTimeTypeInfo,
-//          new HiveIntervalDayTime(0, 0, 0, bdSeconds.intValueExact(),
-//              bdNanos.multiply(NANOS_PER_SEC_BD).intValue()));
-//    default:
-//      throw new IllegalArgumentException("Invalid time literal type " + expr.getType());
-//  }
+    @Override
+    public Integer getKey() {
+      return HiveParser.TOK_INTERVAL_YEAR_LITERAL;
+    }
+
+    @Override
+    protected HiveIntervalYearMonth getIntervalYearMonth(String arg) {
+      return new HiveIntervalYearMonth(Integer.parseInt(arg),0);
+    }
+  }
+
+  private static class IntervalMonthLiteralProcessor extends AbstractYearMonthIntervalProcessor {
+
+    @Override
+    public Integer getKey() {
+      return HiveParser.TOK_INTERVAL_MONTH_LITERAL;
+    }
+
+    @Override
+    protected HiveIntervalYearMonth getIntervalYearMonth(String arg) {
+      return new HiveIntervalYearMonth(0, Integer.parseInt(arg));
+    }
+  }
+
+  private static Map<Integer, IntervalProcessor> getProcessorMap() {
+    
+    Map<Integer, IntervalProcessor> ret = new HashMap<>();
+    IntervalProcessor ips[]=new IntervalProcessor[]{
+        new IntervalDayTimeLiteralProcessor(),
+        
+        new IntervalDayLiteralProcessor(),
+        new IntervalHourLiteralProcessor(),
+        new IntervalMinuteLiteralProcessor(),
+        new IntervalSecondLiteralProcessor(),
+        
+        new IntervalYearMonthLiteralProcessor(),
+        
+        new IntervalYearLiteralProcessor(),
+        new IntervalMonthLiteralProcessor(),
+    };
+    
+    for (IntervalProcessor ip : ips) {
+      ret.put(ip.getKey(), ip);
+    }
+
+    // switch (expr.getType()) {
+//    // case HiveParser.TOK_INTERVAL_YEAR_MONTH_LITERAL:
+    // return new ExprNodeConstantDesc(TypeInfoFactory.intervalYearMonthTypeInfo,
+    // HiveIntervalYearMonth.valueOf(intervalString));
+//    // case HiveParser.TOK_INTERVAL_DAY_TIME_LITERAL:
+    // return new ExprNodeConstantDesc(TypeInfoFactory.intervalDayTimeTypeInfo,
+    // HiveIntervalDayTime.valueOf(intervalString));
+  //  // case HiveParser.TOK_INTERVAL_YEAR_LITERAL:
+    // return new ExprNodeConstantDesc(TypeInfoFactory.intervalYearMonthTypeInfo,
+    // new HiveIntervalYearMonth(Integer.parseInt(intervalString), 0));
+//    // case HiveParser.TOK_INTERVAL_MONTH_LITERAL:
+    // return new ExprNodeConstantDesc(TypeInfoFactory.intervalYearMonthTypeInfo,
+    // new HiveIntervalYearMonth(0, Integer.parseInt(intervalString)));
+//    // case HiveParser.TOK_INTERVAL_DAY_LITERAL:
+    // return new ExprNodeConstantDesc(TypeInfoFactory.intervalDayTimeTypeInfo,
+    // // FIXME HIVE-13557 remove this hoax
+    // new HiveIntervalDayTime(Integer.parseInt("42"), 0, 0, 0, 0));
+//    // case HiveParser.TOK_INTERVAL_HOUR_LITERAL:
+    // return new ExprNodeConstantDesc(TypeInfoFactory.intervalDayTimeTypeInfo,
+    // new HiveIntervalDayTime(0, Integer.parseInt(intervalString), 0, 0, 0));
+//    // case HiveParser.TOK_INTERVAL_MINUTE_LITERAL:
+    // return new ExprNodeConstantDesc(TypeInfoFactory.intervalDayTimeTypeInfo,
+    // new HiveIntervalDayTime(0, 0, Integer.parseInt(intervalString), 0, 0));
+    // case HiveParser.TOK_INTERVAL_SECOND_LITERAL:
+    // BigDecimal bd = new BigDecimal(intervalString);
+    // BigDecimal bdSeconds = new BigDecimal(bd.toBigInteger());
+    // BigDecimal bdNanos = bd.subtract(bdSeconds);
+    // return new ExprNodeConstantDesc(TypeInfoFactory.intervalDayTimeTypeInfo,
+    // new HiveIntervalDayTime(0, 0, 0, bdSeconds.intValueExact(),
+    // bdNanos.multiply(NANOS_PER_SEC_BD).intValue()));
+    // default:
+//    // throw new IllegalArgumentException("Invalid time literal type " + expr.getType());
+    // }
 
     return ret;
   }
--- ql/src/test/queries/clientpositive/interval_alt.q
+++ ql/src/test/queries/clientpositive/interval_alt.q
@@ -1,7 +1,11 @@
 
 select
+	(1) second,
+	(1) minute,
 	(1) hour,
-	(1) day;
+	(1) day,
+	(1) month,
+	(1) year;
 
 select date '2012-01-01' + (30) day;
 select date '2012-01-01' + (-30) day;
@@ -9,5 +13,5 @@ select date '2012-01-01' + (-30) day;
 create table t (dt int);
 insert into t values (1),(2);
 
-select date '2012-01-01' + (dt) day from t;
+select date '2012-01-01' + (dt*dt) day from t;
 
