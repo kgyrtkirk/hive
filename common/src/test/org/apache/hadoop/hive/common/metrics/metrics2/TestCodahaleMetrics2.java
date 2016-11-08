714abf17d54a6a6c8482064c998778337db8e15f
Zoltan Haindrich <zhaindrich@hortonworks.com>
2016-11-08 16:04:51 +0100

    add description
    Change-Id: Idd4a306e258824478de3df3fa33c3cd38f0d39ef

--- common/src/test/org/apache/hadoop/hive/common/metrics/metrics2/TestCodahaleMetrics2.java
+++ common/src/test/org/apache/hadoop/hive/common/metrics/metrics2/TestCodahaleMetrics2.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class TestCodahaleMetrics2 {
 
+  // HIVE-15141: test if close / reconstruct of CodahaleMetrics is ok...earlier there was an exception in case HADOOP2 metrics reporter is used.
   @Test
   public void testCloseAndReopenIsWorking() throws Exception {
     HiveConf conf = new HiveConf();
@@ -15,7 +16,6 @@ public class TestCodahaleMetrics2 {
     cm.close();
     cm = new CodahaleMetrics(conf);
     cm.close();
-
   }
 
 }
