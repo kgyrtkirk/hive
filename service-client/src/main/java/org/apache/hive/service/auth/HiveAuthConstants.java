6d5f8a42dee261a2943cad9f2238d127cb3678d5
Zoltan Haindrich <kirk@rxd.hu>
2017-04-10 23:07:27 +0200

    prepare to move constants

---
+++ service-client/src/main/java/org/apache/hive/service/auth/HiveAuthConstants.java
@@ -0,0 +1,6 @@
+package org.apache.hive.service.auth;
+
+public class HiveAuthConstants {
+  public static final String HS2_PROXY_USER = "hive.server2.proxy.user";
+  public static final String HS2_CLIENT_TOKEN = "hiveserver2ClientToken";
+}
