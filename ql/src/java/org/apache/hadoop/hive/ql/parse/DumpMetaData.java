7c00e7d04caf58e926e9d65e246323f635628498
Zoltan Haindrich <kirk@rxd.hu>
2017-04-14 00:39:37 +0200

    INDEP: move Dump enum

--- ql/src/java/org/apache/hadoop/hive/ql/parse/DumpMetaData.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/DumpMetaData.java
@@ -9,13 +9,39 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.exec.Utilities;
-import org.apache.hadoop.hive.ql.parse.ReplicationSemanticAnalyzer.DUMPTYPE;
+import org.apache.hadoop.hive.ql.parse.DumpMetaData.DUMPTYPE;
 
 public class DumpMetaData {
   // wrapper class for reading and writing metadata about a dump
   // responsible for _dumpmetadata files
 
-  private DUMPTYPE dumpType;
+  public enum DUMPTYPE {
+    BOOTSTRAP("BOOTSTRAP"),
+    INCREMENTAL("INCREMENTAL"),
+    EVENT_CREATE_TABLE("EVENT_CREATE_TABLE"),
+    EVENT_ADD_PARTITION("EVENT_ADD_PARTITION"),
+    EVENT_DROP_TABLE("EVENT_DROP_TABLE"),
+    EVENT_DROP_PARTITION("EVENT_DROP_PARTITION"),
+    EVENT_ALTER_TABLE("EVENT_ALTER_TABLE"),
+    EVENT_RENAME_TABLE("EVENT_RENAME_TABLE"),
+    EVENT_ALTER_PARTITION("EVENT_ALTER_PARTITION"),
+    EVENT_RENAME_PARTITION("EVENT_RENAME_PARTITION"),
+    EVENT_INSERT("EVENT_INSERT"),
+    EVENT_UNKNOWN("EVENT_UNKNOWN");
+  
+    String type = null;
+    DUMPTYPE(String type) {
+      this.type = type;
+    }
+  
+    @Override
+    public String toString(){
+      return type;
+    }
+  
+  }
+
+  private DumpMetaData.DUMPTYPE dumpType;
   private Long eventFrom = null;
   Long eventTo = null;
   private String payload = null;
@@ -32,13 +58,13 @@ public class DumpMetaData {
     dumpFile = new Path(dumpRoot, ReplicationSemanticAnalyzer.DUMPMETADATA);
   }
 
-  public DumpMetaData(Path dumpRoot, DUMPTYPE lvl, Long eventFrom, Long eventTo, Path cmRoot,
+  public DumpMetaData(Path dumpRoot, DumpMetaData.DUMPTYPE lvl, Long eventFrom, Long eventTo, Path cmRoot,
       HiveConf hiveConf){
     this(dumpRoot,hiveConf);
     setDump(lvl, eventFrom, eventTo, cmRoot);
   }
 
-  public void setDump(DUMPTYPE lvl, Long eventFrom, Long eventTo, Path cmRoot){
+  public void setDump(DumpMetaData.DUMPTYPE lvl, Long eventFrom, Long eventTo, Path cmRoot){
     this.dumpType = lvl;
     this.eventFrom = eventFrom;
     this.eventTo = eventTo;
@@ -54,7 +80,7 @@ public class DumpMetaData {
       String line = null;
       if ( (line = br.readLine()) != null){
         String[] lineContents = line.split("\t", 5);
-        setDump(DUMPTYPE.valueOf(lineContents[0]), Long.valueOf(lineContents[1]), Long.valueOf(lineContents[2]),
+        setDump(DumpMetaData.DUMPTYPE.valueOf(lineContents[0]), Long.valueOf(lineContents[1]), Long.valueOf(lineContents[2]),
             new Path(lineContents[3]));
         setPayload(lineContents[4].equals(Utilities.nullStringOutput) ? null : lineContents[4]);
         ReplChangeManager.setCmRoot(cmRoot);
@@ -66,7 +92,7 @@ public class DumpMetaData {
     }
   }
 
-  public DUMPTYPE getDumpType() throws SemanticException {
+  public DumpMetaData.DUMPTYPE getDumpType() throws SemanticException {
     initializeIfNot();
     return this.dumpType;
   }
@@ -104,7 +130,7 @@ public class DumpMetaData {
 
   public boolean isIncrementalDump() throws SemanticException {
     initializeIfNot();
-    return (this.dumpType == DUMPTYPE.INCREMENTAL);
+    return (this.dumpType == DumpMetaData.DUMPTYPE.INCREMENTAL);
   }
 
   private void initializeIfNot() throws SemanticException {
