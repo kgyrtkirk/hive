4c43684b79c828367557994f2ad3b0c017d91461
Zoltan Haindrich <kirk@rxd.hu>
2017-04-20 07:25:21 +0200

    Revert "INDEP: move DumpMetaData"
    This reverts commit 6f4622c3c2883da65fe76fcf292c01ab9f2e06cd.

--- ql/src/java/org/apache/hadoop/hive/ql/parse/ReplicationSemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/ReplicationSemanticAnalyzer.java
@@ -78,11 +78,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
-
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Serializable;
 import java.net.URI;
@@ -143,6 +144,123 @@ public class ReplicationSemanticAnalyzer extends BaseSemanticAnalyzer {
     }
 
   };
+
+  public static class DumpMetaData {
+    // wrapper class for reading and writing metadata about a dump
+    // responsible for _dumpmetadata files
+
+    private DUMPTYPE dumpType;
+    private Long eventFrom = null;
+    private Long eventTo = null;
+    private String payload = null;
+    private boolean initialized = false;
+
+    private final Path dumpRoot;
+    private final Path dumpFile;
+    private final HiveConf hiveConf;
+    private Path cmRoot;
+
+    public DumpMetaData(Path dumpRoot, HiveConf hiveConf) {
+      this.dumpRoot = dumpRoot;
+      this.hiveConf = hiveConf;
+      dumpFile = new Path(dumpRoot, DUMPMETADATA);
+    }
+
+    public DumpMetaData(Path dumpRoot, DUMPTYPE lvl, Long eventFrom, Long eventTo, Path cmRoot,
+        HiveConf hiveConf){
+      this(dumpRoot,hiveConf);
+      setDump(lvl, eventFrom, eventTo, cmRoot);
+    }
+
+    public void setDump(DUMPTYPE lvl, Long eventFrom, Long eventTo, Path cmRoot){
+      this.dumpType = lvl;
+      this.eventFrom = eventFrom;
+      this.eventTo = eventTo;
+      this.initialized = true;
+      this.cmRoot = cmRoot;
+    }
+
+    public void loadDumpFromFile() throws SemanticException {
+      try {
+        // read from dumpfile and instantiate self
+        FileSystem fs = dumpFile.getFileSystem(hiveConf);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(dumpFile)));
+        String line = null;
+        if ( (line = br.readLine()) != null){
+          String[] lineContents = line.split("\t", 5);
+          setDump(DUMPTYPE.valueOf(lineContents[0]), Long.valueOf(lineContents[1]), Long.valueOf(lineContents[2]),
+              new Path(lineContents[3]));
+          setPayload(lineContents[4].equals(Utilities.nullStringOutput) ? null : lineContents[4]);
+          ReplChangeManager.setCmRoot(cmRoot);
+        } else {
+          throw new IOException("Unable to read valid values from dumpFile:"+dumpFile.toUri().toString());
+        }
+      } catch (IOException ioe){
+        throw new SemanticException(ioe);
+      }
+    }
+
+    public DUMPTYPE getDumpType() throws SemanticException {
+      initializeIfNot();
+      return this.dumpType;
+    }
+
+    public String getPayload() throws SemanticException {
+      initializeIfNot();
+      return this.payload;
+    }
+
+    public void setPayload(String payload) {
+      this.payload = payload;
+    }
+
+    public Long getEventFrom() throws SemanticException {
+      initializeIfNot();
+      return eventFrom;
+    }
+
+    public Long getEventTo() throws SemanticException {
+      initializeIfNot();
+      return eventTo;
+    }
+
+    public Path getCmRoot() {
+      return cmRoot;
+    }
+
+    public void setCmRoot(Path cmRoot) {
+      this.cmRoot = cmRoot;
+    }
+
+    public Path getDumpFilePath() {
+      return dumpFile;
+    }
+
+    public boolean isIncrementalDump() throws SemanticException {
+      initializeIfNot();
+      return (this.dumpType == DUMPTYPE.INCREMENTAL);
+    }
+
+    private void initializeIfNot() throws SemanticException {
+      if (!initialized){
+        loadDumpFromFile();
+      }
+    }
+
+    public void write() throws SemanticException {
+      writeOutput(
+          Arrays.asList(
+              dumpType.toString(),
+              eventFrom.toString(),
+              eventTo.toString(),
+              cmRoot.toString(),
+              payload),
+          dumpFile,
+          hiveConf
+      );
+    }
+
+  }
 
   public ReplicationSemanticAnalyzer(QueryState queryState) throws SemanticException {
     super(queryState);
@@ -1071,7 +1189,7 @@ public class ReplicationSemanticAnalyzer extends BaseSemanticAnalyzer {
     writeOutput(values, ctx.getResFile(), conf);
   }
 
-  static void writeOutput(List<String> values, Path outputFile, HiveConf hiveConf)
+  private static void writeOutput(List<String> values, Path outputFile, HiveConf hiveConf)
       throws SemanticException {
     FileSystem fs = null;
     DataOutputStream outStream = null;
