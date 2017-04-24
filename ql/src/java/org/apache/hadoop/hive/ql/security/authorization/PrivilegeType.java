cc9f01d229a68487a8680b5db9b51d7f91b5fd00
Zoltan Haindrich <kirk@rxd.hu>
2017-04-24 21:09:36 +0200

    HIVE-16500

--- ql/src/java/org/apache/hadoop/hive/ql/security/authorization/PrivilegeType.java
+++ ql/src/java/org/apache/hadoop/hive/ql/security/authorization/PrivilegeType.java
@@ -21,32 +21,28 @@ package org.apache.hadoop.hive.ql.security.authorization;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.hadoop.hive.ql.parse.HiveParser;
-
 /**
  * Privilege type
  */
 public enum PrivilegeType {
 
-  ALL(HiveParser.TOK_PRIV_ALL, "All"),
-  ALTER_DATA(HiveParser.TOK_PRIV_ALTER_DATA, "Update"),
-  ALTER_METADATA(HiveParser.TOK_PRIV_ALTER_METADATA, "Alter"),
-  CREATE(HiveParser.TOK_PRIV_CREATE, "Create"),
-  DROP(HiveParser.TOK_PRIV_DROP, "Drop"),
-  INDEX(HiveParser.TOK_PRIV_INDEX, "Index"),
-  LOCK(HiveParser.TOK_PRIV_LOCK, "Lock"),
-  SELECT(HiveParser.TOK_PRIV_SELECT, "Select"),
-  SHOW_DATABASE(HiveParser.TOK_PRIV_SHOW_DATABASE, "Show_Database"),
-  INSERT(HiveParser.TOK_PRIV_INSERT, "Insert"),
-  DELETE(HiveParser.TOK_PRIV_DELETE, "Delete"),
-  UNKNOWN(null, null);
+  ALL("All"),
+  ALTER_DATA("Update"),
+  ALTER_METADATA("Alter"),
+  CREATE("Create"),
+  DROP("Drop"),
+  INDEX("Index"),
+  LOCK("Lock"),
+  SELECT("Select"),
+  SHOW_DATABASE("Show_Database"),
+  INSERT("Insert"),
+  DELETE("Delete"),
+  UNKNOWN(null);
 
   private final String name;
-  private final Integer token;
 
-  PrivilegeType(Integer token, String name){
+  PrivilegeType(String name){
     this.name = name;
-    this.token = token;
   }
 
   @Override
@@ -54,36 +50,7 @@ public enum PrivilegeType {
     return name == null ? "unkown" : name;
   }
 
-  public Integer getToken() {
-    return token;
-  }
-
-  private static Map<Integer, PrivilegeType> token2Type;
   private static Map<String, PrivilegeType> name2Type;
-
-  /**
-   * Do case lookup of PrivilegeType associated with this antlr token
-   * @param privilegeName
-   * @return corresponding PrivilegeType
-   */
-  public static PrivilegeType getPrivTypeByToken(int token) {
-    populateToken2Type();
-    PrivilegeType privType = token2Type.get(token);
-    if(privType != null){
-      return privType;
-    }
-    return PrivilegeType.UNKNOWN;
-  }
-
-  private static synchronized void populateToken2Type() {
-    if(token2Type != null){
-      return;
-    }
-    token2Type = new HashMap<Integer, PrivilegeType>();
-    for(PrivilegeType privType : PrivilegeType.values()){
-      token2Type.put(privType.getToken(), privType);
-    }
-  }
 
   /**
    * Do case insensitive lookup of PrivilegeType with this name
