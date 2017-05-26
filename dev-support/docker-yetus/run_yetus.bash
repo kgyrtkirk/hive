cf7b69f3d497cac43092025d3cc7764e3974fe4f
Zoltan Haindrich <kirk@rxd.hu>
2017-05-26 11:52:03 +0200

    make it a bit better; dont fail on sparkDl; support URL; add nginx....

--- dev-support/docker-yetus/run_yetus.bash
+++ dev-support/docker-yetus/run_yetus.bash
@@ -1,10 +1,17 @@
 #!/bin/bash -e
 
+PATCH_URL="$1"
+PATCH_FILE="/`basename "$PATCH_URL"`"
+shift
+
+echo "*** update hive repo"
 cd hive
 git clean -dfx
 git checkout master
 git pull
+echo "*** fetching patch($PATCH_URL)"
+wget -O "$PATCH_FILE" "$PATCH_URL"
 echo "*** preflight mvn install"
 time mvn install -q -T4 -DskipTests -Pitests -DskipSparkTests
 echo "*** run yetus"
-test-patch --personality=/hive-personality.sh --skip-dir=dev-support "$@"
+test-patch --personality=/hive-personality.sh --skip-dir=dev-support "$PATCH_FILE" "$@"
--- dev-support/test-patch2.sh
+++ dev-support/test-patch2.sh
@@ -1,13 +1,31 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
-[ "$1" == "" ] && echo "usage: $0 <patch_file>" && exit 1
+[ "$1" == "" ] && echo "usage: $0 <patch_file|patch_url> [more yetus args]" && exit 1
 
-#container="`docker create yh`"
-#docker cp "$1" $container:/
 PF="$1"
-[ "${PF:0:1}" == "." ] && PF="`pwd`/$PF"
+shift
+
+#[ "${PF:0:1}" == "." ] && PF="`pwd`/$PF"
 PN="`basename "$PF"`"
 echo $PN
 
-#docker run --rm -it -v ~/.m2:/root/.m2 -v "$PF:/$PN" yh /run_yetus.bash "/$PN"
-docker run --rm -it -v ~/.m2/settings.xml:/root/.m2/settings.xml -v "$PF:/$PN" yh /run_yetus.bash "/$PN"
+# this part will launch an nginx; the tested patch will be loaded into it
+nginx_name='hive_yetus_nginx'
+if [ "$(docker ps -q -f "name=^/$nginx_name$")" == "" ]; then
+	echo "* launch $nginx_name"
+	docker run --restart=always --name $nginx_name -d nginx
+fi
+
+# the yetus test container expects an URL, so if its a local file; will load it into the nginx
+if [ "${PF//http*/}" == "" ];then
+	PATCH_URL="$PF"
+else
+	docker cp "$PF" $nginx_name:"/usr/share/nginx/html/$PN"
+	PATCH_URL="http://$nginx_name/$PN"
+fi
+
+docker run --rm -it 		\
+	--link hive_yetus_nginx	\
+	-v ~/.m2/settings.xml:/root/.m2/settings.xml \
+	yh \
+	/run_yetus.bash "$PATCH_URL" "$@"
