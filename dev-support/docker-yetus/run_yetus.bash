d524808b9b25980a9f2eea06d0e8c9b6b69c5972
Zoltan Haindrich <kirk@rxd.hu>
2017-05-26 07:53:46 +0200

    updates

--- dev-support/docker-yetus/run_yetus.bash
+++ dev-support/docker-yetus/run_yetus.bash
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 
 cd hive
 git fetch
 git checkout master
+echo "*** preflight mvn install"
+time mvn install -q -T4 -DskipTests -Pitests -DskipSparkTests
+echo "*** run yetus"
 test-patch --personality=/hive-personality.sh --skip-dir=dev-support "$@"
--- dev-support/test-patch2.sh
+++ dev-support/test-patch2.sh
@@ -2,9 +2,12 @@
 
 [ "$1" == "" ] && echo "usage: $0 <patch_file>" && exit 1
 
-container="`docker create yh`"
-docker cp "$1" $container:/
-docker exec -it $container -- /run_yetus.bash "`basename "$1"`"
-r=$?
-docker rm "$container"
-exit $r
+#container="`docker create yh`"
+#docker cp "$1" $container:/
+PF="$1"
+[ "${PF:0:1}" == "." ] && PF="`pwd`/$PF"
+PN="`basename "$PF"`"
+echo $PN
+
+#docker run --rm -it -v ~/.m2:/root/.m2 -v "$PF:/$PN" yh /run_yetus.bash "/$PN"
+docker run --rm -it -v ~/.m2/settings.xml:/root/.m2/settings.xml -v "$PF:/$PN" yh /run_yetus.bash "/$PN"
