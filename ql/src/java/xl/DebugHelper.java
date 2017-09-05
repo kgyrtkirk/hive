d30e0e73ed132c1cb0c147f9e0fecb1b598d46ed
Zoltan Haindrich <kirk@rxd.hu>
2017-09-05 10:08:27 +0200

    add DebugHelper

---
+++ ql/src/java/xl/DebugHelper.java
@@ -0,0 +1,10 @@
+package xl;
+
+public class DebugHelper {
+  public static String query = null;
+  public static boolean interesting = false;
+  public static int i0 = 0;
+  public static int i1 = 0;
+  public static int i2 = 0;
+
+}
