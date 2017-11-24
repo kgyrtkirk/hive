905a508a1dbd72a087d365d35684e33e69dc3fc2
Zoltan Haindrich <kirk@rxd.hu>
2017-11-24 15:19:17 +0100

    remove comment

--- ql/src/java/org/apache/hadoop/hive/ql/stats/StatsUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/stats/StatsUtils.java
@@ -360,12 +360,6 @@ public class StatsUtils {
       stats = new Statistics(nr, ds);
       stats.setBasicStatsState(bbs.getState());
 
-      // if at least a partition does not contain row count then mark basic stats state as PARTIAL
-//      if (containsNonPositives(rowCounts) &&
-//          stats.getBasicStatsState().equals(State.COMPLETE)) {
-//        stats.setBasicStatsState(State.PARTIAL);
-//      }
-
       if (fetchColStats) {
         List<String> partitionCols = getPartitionColumns(
             schema, neededColumns, referencedColumns);
