06156b5d4fda481cdd38f4b5cc5bd89e6021e744
Zoltan Haindrich <kirk@rxd.hu>
2017-12-07 15:10:28 +0100

    add X

--- ql/src/java/org/apache/hadoop/hive/ql/DriverFactory.java
+++ ql/src/java/org/apache/hadoop/hive/ql/DriverFactory.java
@@ -18,10 +18,17 @@
 
 package org.apache.hadoop.hive.ql;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.session.SessionState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DriverFactory {
+
+  private static boolean mmm;
 
   public static IDriver newDriver(HiveConf conf) {
     return newDriver(getNewQueryState(conf), null, null);
@@ -43,9 +50,70 @@ public class DriverFactory {
     return strategy.build(queryState, userName, queryInfo);
   }
 
+  public static class ProtectedHiveConf extends HiveConf {
+
+    private static transient final Logger LOG = LoggerFactory.getLogger(ProtectedHiveConf.class);
+
+    private boolean protecting = false;
+    private HiveConf conf;
+    private Set<String> rp;
+
+    public ProtectedHiveConf(HiveConf conf) {
+      super(conf);
+      this.conf = conf;
+      protecting = true;
+      rp = new HashSet<>();
+
+      //      rp.add("fs.scheme.class");
+      //      rp.add("hive.exec.dynamic.partition.mode");
+      //      rp.add("hive.mapred.mode");
+      //      rp.add("hive.query.id");
+      //      rp.add("hive.query.string");
+      //      rp.add("hive.rpc.query.plan");
+      //      rp.add("io.file.buffer.size");
+      //      rp.add("mapred.input.dir.recursive");
+      //      rp.add("mapred.mapper.new-api");
+      //      rp.add("mapreduce.job.name");
+      //      rp.add("mapreduce.workflow.id");
+      //      rp.add("mapreduce.workflow.name");
+      //      rp.add("mapreduce.workflow.node.name");
+
+    }
+
+    @Override
+    public void set(String arg0, String arg1, String arg2) {
+      if (protecting) {
+        LOG.error("protected#1; {} but set: {} = {}", reallyProtected(arg0), arg0, arg1);
+      }
+      if (!reallyProtected(arg0)) {
+        conf.set(arg0, arg1, arg2);
+      }
+      super.set(arg0, arg1, arg2);
+    }
+
+    private boolean reallyProtected(String arg0) {
+      return rp.contains(arg0);
+    }
+
+    @Override
+    public void set(String name, String value) {
+      if (protecting) {
+        LOG.error("protected#2; {} but set: {} = {}", reallyProtected(name), name, value);
+      }
+      if (!reallyProtected(name)) {
+        conf.set(name, value);
+      }
+      super.set(name, value);
+    }
+
+  }
+
   private static QueryState getNewQueryState(HiveConf conf) {
     // async=true techincally forces to enable HiveConf isolations; but instead for now explicitly:
-    HiveConf newConf = new HiveConf(conf);
+    HiveConf newConf = new ProtectedHiveConf(conf);
+    if (mmm) {
+      newConf = conf;
+    }
     return new QueryState.Builder().withGenerateNewQueryId(true).withHiveConf(newConf).build();
   }
 
@@ -55,6 +123,10 @@ public class DriverFactory {
     // CLIDriver enter at this point
     HiveConf conf = (SessionState.get() != null) ? SessionState.get().getConf() : new HiveConf();
     return newDriver(conf);
+  }
+
+  public static void setFTsetMode(boolean b) {
+    mmm = b;
   }
 
 }
