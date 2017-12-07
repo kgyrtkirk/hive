f862a9f1d6f06001fb171bea48254762b6314fcf
Zoltan Haindrich <kirk@rxd.hu>
2017-12-07 13:24:32 +0100

    Revert "trials to detect changed hiveconf values"
    This reverts commit 4db3302aa7490da7c5d91d2dcbe45aa7d73e4511.

--- ql/src/java/org/apache/hadoop/hive/ql/DriverFactory.java
+++ ql/src/java/org/apache/hadoop/hive/ql/DriverFactory.java
@@ -18,17 +18,8 @@
 
 package org.apache.hadoop.hive.ql;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.util.HashSet;
-import java.util.Set;
-
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.session.SessionState;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javassist.util.proxy.MethodHandler;
 
 public class DriverFactory {
 
@@ -54,115 +45,8 @@ public class DriverFactory {
 
   private static QueryState getNewQueryState(HiveConf conf) {
     // async=true techincally forces to enable HiveConf isolations; but instead for now explicitly:
-    HiveConf newConf = protectedHiveConf(conf);
+    HiveConf newConf = new HiveConf(conf);
     return new QueryState.Builder().withGenerateNewQueryId(true).withHiveConf(newConf).build();
-  }
-
-  static class HiveConfProtector implements InvocationHandler, MethodHandler {
-
-    private static final Logger LOG = LoggerFactory.getLogger(HiveConfProtector.class);
-
-    private HiveConf conf;
-
-    private HiveConfProtector(HiveConf conf) {
-      this.conf = conf;
-    }
-
-    @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-      if (method.getName().startsWith("set")) {
-        LOG.error("invoced set on ro {}", method.getName());
-      }
-      return method.invoke(conf, args);
-    }
-
-    @Override
-    public Object invoke(Object self, Method thisMethod, Method proceed, Object[] args) throws Throwable {
-      return invoke(self, thisMethod, args);
-    }
-
-  }
-
-  public static class ProtectedHiveConf extends HiveConf {
-
-    private static final Logger LOG = LoggerFactory.getLogger(ProtectedHiveConf.class);
-    private boolean protecting = false;
-    private HiveConf conf;
-    private Set<String> rp;
-
-    public ProtectedHiveConf(HiveConf conf) {
-      super(conf);
-      this.conf = conf;
-      protecting = true;
-      rp = new HashSet<>();
-
-      rp.add("fs.scheme.class");
-      rp.add("hive.exec.dynamic.partition.mode");
-      rp.add("hive.mapred.mode");
-      rp.add("hive.query.id");
-      rp.add("hive.query.string");
-      rp.add("hive.rpc.query.plan");
-      rp.add("io.file.buffer.size");
-      rp.add("mapred.input.dir.recursive");
-      rp.add("mapred.mapper.new-api");
-      rp.add("mapreduce.job.name");
-      rp.add("mapreduce.workflow.id");
-      rp.add("mapreduce.workflow.name");
-      rp.add("mapreduce.workflow.node.name");
-
-    }
-
-    @Override
-    public void set(String arg0, String arg1, String arg2) {
-      if (protecting) {
-        LOG.error("protected#1; {} but set: {} = {}", reallyProtected(arg0), arg0, arg1);
-      }
-      if (!reallyProtected(arg0)) {
-        conf.set(arg0, arg1, arg2);
-      }
-      super.set(arg0, arg1, arg2);
-    }
-
-    private boolean reallyProtected(String arg0) {
-      return rp.contains(arg0);
-    }
-
-    @Override
-    public void set(String name, String value) {
-      if (protecting) {
-        LOG.error("protected#2; {} but set: {} = {}", reallyProtected(name), name, value);
-      }
-      if (!reallyProtected(name)) {
-        conf.set(name, value);
-      }
-      super.set(name, value);
-    }
-
-  }
-
-  static HiveConf protectedHiveConf(HiveConf conf) {
-//    return new ProtectedHiveConf(conf);
-    return conf;
-    //    HiveConfProtector h = new HiveConfProtector(conf);
-    //    ProxyFactory pf = new ProxyFactory();
-    //    pf.setFilter(new MethodFilter() {
-    //
-    //      @Override
-    //      public boolean isHandled(Method m) {
-    //        if (m.getName().startsWith("_")) {
-    //          return false;
-    //        }
-    //        return true;
-    //      }
-    //    });
-    //    pf.setSuperclass(HiveConf.class);
-    //    try {
-    //      return (HiveConf) pf.create(new Class<?>[0], new Object[0], h);
-    //    } catch (NoSuchMethodException | IllegalArgumentException | InstantiationException | IllegalAccessException
-    //        | InvocationTargetException e) {
-    //      throw new RuntimeException(e);
-    //    }
-    //    return (IHiveConf) Proxy.newProxyInstance(HiveConf.class.getClassLoader(), new Class[] { IHiveConf.class }, h);
   }
 
   // it would be better to use conf at the callsite...but instead for now I clone the original magic from Driver
