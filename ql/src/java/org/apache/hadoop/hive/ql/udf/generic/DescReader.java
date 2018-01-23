35d841f5f670c6d96471b454b3f775f75a1b01a3
Zoltan Haindrich <kirk@rxd.hu>
2018-01-23 10:36:48 +0100

    update reader

--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/DescReader.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/DescReader.java
@@ -3,26 +3,10 @@ package org.apache.hadoop.hive.ql.udf.generic;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.hadoop.hive.ql.metadata.HiveException;
-import org.apache.hadoop.hive.serde2.objectinspector.ListObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorConverters;
-import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorConverters.Converter;
-import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.StructField;
-import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
-import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
-import org.apache.hadoop.hive.serde2.typeinfo.TypeInfo;
-import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoUtils;
-
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonToken;
+import com.google.common.base.Joiner;
 
 public class DescReader {
 
@@ -41,174 +25,120 @@ public class DescReader {
   static class DRX {
 
     private List<Col> cols = new ArrayList<>();
+    private String tableName;
+    private List<Col> parts = new ArrayList<>();
 
-    public void add(Col col) {
-      cols.add(col);
+    public DRX(String tableName) {
+      this.tableName = tableName;
     }
 
-  }
-
-  public static void main(String[] args) throws HiveException {
-    String carJson = "{ \"brand\" : \"Mercedes\", \"doors\":5 , \"s\":[{\"x\":99}] }";
-
-    TypeInfo t = TypeInfoUtils.getTypeInfoFromTypeString("struct<brand:string,doors:int,s:array<struct<x:string>>>");
-    StructObjectInspector oi = (StructObjectInspector) TypeInfoUtils.getStandardWritableObjectInspectorFromTypeInfo(t);
-
-    JsonFactory factory = new JsonFactory();
-    JsonParser parser;
-    try {
-      //      ComplexObjectConstructor coc = new ComplexObjectConstructor(oi);
-      parser = factory.createParser(carJson);
-
-      while (!parser.isClosed()) {
-        JsonToken jsonToken = parser.nextToken();
-        //              if (jsonToken != null) {
-        //                switch (jsonToken) {
-        //                case START_OBJECT:
-        //                  coc.startObject();
-        //                }
-        //              }
-        System.out.println("jsonToken = " + jsonToken);
+    public void add(Col col, boolean partition2) {
+      if(partition2) {
+        parts.add(col);
+      } else {
+        cols.add(col);
       }
-      parser = factory.createParser(carJson);
-
-      parser.nextToken();
-      Object res = parseDispatcher(parser, oi);
-
-    } catch (IOException e) {
-      throw new HiveException("error parsing", e);
     }
-    System.out.println(oi);
 
-  }
+    public String buildSrc() {
+      StringBuilder sb = new StringBuilder("create table ");
+      sb.append(tableName);
 
-  //  static class ComplexObjectConstructor {
-  //
-  //    private ObjectInspector rootOI;
-  //    private ObjectInspector ois;
-  //
-  //    public ComplexObjectConstructor(ObjectInspector oi) {
-  //      this.rootOI = oi;
-  //    }
-  //
-  //    public void startObject() {
-  //      if()
-  //    }
-  //
-  //
-  //  }
-  //
-  // probably forget the parser as an argument
-  private static Object parseDispatcher(JsonParser parser, ObjectInspector oi)
-      throws JsonParseException, IOException, HiveException {
+      sb.append(" (\n  " + getColsDemoteComplexToString(cols) + ")\n ");
+      if (parts.size() != 0) {
+        sb.append("partitioned by (\n  " + getColsDemoteComplexToString(parts) + ")\n ");
+      }
+      sb.append(";");
 
-    switch (oi.getCategory()) {
-    case PRIMITIVE:
-      return parsePrimitive(parser, (PrimitiveObjectInspector) oi);
-    case LIST:
-      return parseList(parser, (ListObjectInspector) oi);
-    case STRUCT:
-      return parseStruct(parser, (StructObjectInspector) oi);
-    default:
-      throw new HiveException("parsing of: " + oi.getCategory() + " is not handled");
+
+      return sb.toString();
     }
-  }
 
-  private static Object parseStruct(JsonParser parser, StructObjectInspector oi)
-      throws JsonParseException, IOException, HiveException {
-
-    Object ret[] = new Object[oi.getAllStructFieldRefs().size()];
-
-    if (!parser.isExpectedStartObjectToken()) {
-      throw new HiveException("struct expected");
-    }
-    JsonToken currentToken = parser.nextToken();
-    while (currentToken != null && currentToken != JsonToken.END_OBJECT) {
-
-      switch (currentToken) {
-      case FIELD_NAME:
-        String name = parser.getCurrentName();
-        StructField field = oi.getStructFieldRef(name);
-        if (field == null) {
-          throw new HiveException("field with name: " + name + " is unknown");
+    private String getColsDemoteComplexToString(List<Col> parts2) {
+      List<String> c1 = new ArrayList<>();
+      for (Col col : parts2) {
+        if (isComplex(col.type)) {
+          c1.add(col.name + " string");
+        } else {
+          c1.add(col.name + " " + col.type);
         }
-        parser.nextToken();
-        ret[field.getFieldID()] = parseDispatcher(parser, field.getFieldObjectInspector());
-        break;
-      default:
-        throw new HiveException("unexpected token: " + currentToken);
       }
-      currentToken = parser.getCurrentToken();
+      return Joiner.on(",\n  ").join(c1);
     }
-    if (currentToken != null) {
-      parser.nextToken();
-    }
-    return ret;
-  }
 
-  private static Object parseList(JsonParser parser, ListObjectInspector oi)
-      throws JsonParseException, IOException, HiveException {
-    List<Object> ret = new ArrayList<>();
-    if (!parser.isExpectedStartArrayToken()) {
-      throw new HiveException("array expected");
-    }
-    JsonToken currentToken = parser.nextToken();
-    while (currentToken != null && currentToken != JsonToken.END_ARRAY) {
-      ObjectInspector eOI = oi.getListElementObjectInspector();
-      ret.add(parseDispatcher(parser, eOI));
-      currentToken = parser.getCurrentToken();
-    }
-    currentToken = parser.nextToken();
-
-    return ret;
-  }
-
-
-  private static Object parsePrimitive(JsonParser parser, PrimitiveObjectInspector oi)
-      throws HiveException, IOException {
-    JsonToken currentToken = parser.getCurrentToken();
-    if (currentToken == null) {
-      // FIXME: does this makes sense?
-      return null;
-    }
-    try {
-      switch (parser.getCurrentToken()) {
-      case VALUE_FALSE:
-      case VALUE_TRUE:
-      case VALUE_NUMBER_INT:
-      case VALUE_NUMBER_FLOAT:
-      case VALUE_STRING:
-        Converter c =
-            ObjectInspectorConverters.getConverter(PrimitiveObjectInspectorFactory.javaStringObjectInspector, oi);
-        return c.convert(parser.getValueAsString());
-      case VALUE_NULL:
-      default:
-        throw new HiveException("unexpected token type: " + currentToken);
+    public String buildView() {
+      StringBuilder sb = new StringBuilder("create view ");
+      sb.append(tableName);
+      sb.append("_view");
+      if (parts.size() != 0) {
+        sb.append(" partitioned on (" + colNamesOf(parts) + ") ");
       }
-    } finally {
-      parser.nextToken();
+      sb.append(" as \n select\n  ");
+      ArrayList<Col> allCols = new ArrayList<>();
+      allCols.addAll(cols);
+      allCols.addAll(parts);
+      List<String> viewC = new ArrayList<>();
+      for (Col c : allCols) {
+        String colStr;
+        if (isComplex(c.type)) {
+          colStr = String.format("json_parse(%s,'%s') as %s", c.name, c.type, c.name);
+        } else {
+          colStr = c.name;
+        }
+        viewC.add(colStr);
+      }
+      sb.append(Joiner.on(",\n  ").join(viewC));
+      sb.append("\n from " + tableName + ";");
 
+      return sb.toString();
     }
+
+    private boolean isComplex(String type) {
+      return type.contains("array") || type.contains("struct");
+    }
+
+    private String colNamesOf(List<Col> cols) {
+      StringBuffer sb = new StringBuffer();
+      for (Col col : cols) {
+        if (sb.length() > 0) {
+          sb.append(",");
+        }
+        sb.append(col.name);
+      }
+      return sb.toString();
+    }
+
+    public void setPart() {
+    }
+
   }
 
-  public static void main1(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
     List<DRX> tables = new ArrayList<>();
 
     try (BufferedReader br = new BufferedReader(new FileReader("/home/kirk/Downloads/DDL.txt"))) {
       String l;
       DRX drx = null;
+      boolean partition = false;
       while ((l = br.readLine()) != null) {
         l = l.trim();
         if (l.startsWith("hive>")) {
           if (drx != null) {
             throw new RuntimeException("FX: " + l);
           }
-          drx = new DRX();
+          String[] pp = l.split("[ ;]+");
+          if (pp.length != 4) {
+            throw new RuntimeException();
+          }
+          drx = new DRX(pp[3]);
+          partition = false;
           continue;
         }
 
         if (drx != null) {
-          if (l.startsWith("# P") || l.startsWith("# D")) {
+          if (l.startsWith("# P")) {
+            partition = true;
+          } else if (l.startsWith("# D")) {
             tables.add(drx);
             drx = null;
           } else if (l.startsWith("#") || l.startsWith("O") || l.length() == 0) {
@@ -217,10 +147,15 @@ public class DescReader {
             if (parts.length != 2) {
               throw new RuntimeException("EE: " + l);
             }
-            drx.add(new Col(parts[0], parts[1]));
+            drx.add(new Col(parts[0], parts[1]), partition);
           }
         }
       }
+    }
+
+    for (DRX drx : tables) {
+      System.out.println(drx.buildSrc());
+      System.out.println(drx.buildView());
     }
 
   }
