913a1bed3bce0b066f3f6427e36ad33822f7368a
Zoltan Haindrich <kirk@rxd.hu>
2018-02-07 10:34:08 +0100

    remove @Nonnull

--- ql/src/java/org/apache/hadoop/hive/ql/DriverFactory.java
+++ ql/src/java/org/apache/hadoop/hive/ql/DriverFactory.java
@@ -18,8 +18,6 @@
 
 package org.apache.hadoop.hive.ql;
 
-import javax.annotation.Nonnull;
-
 import org.apache.hadoop.hive.conf.HiveConf;
 
 /**
@@ -38,16 +36,16 @@ public class DriverFactory {
     abstract IDriver build(QueryState queryState, String userName, QueryInfo queryInfo);
   }
 
-  public static IDriver newDriver(@Nonnull HiveConf conf) {
+  public static IDriver newDriver(HiveConf conf) {
     return newDriver(getNewQueryState(conf), null, null);
   }
 
-  public static IDriver newDriver(@Nonnull QueryState queryState, String userName, QueryInfo queryInfo) {
+  public static IDriver newDriver(QueryState queryState, String userName, QueryInfo queryInfo) {
     ExecutionStrategy strategy = ExecutionStrategy.none;
     return strategy.build(queryState, userName, queryInfo);
   }
 
-  private static QueryState getNewQueryState(@Nonnull HiveConf conf) {
+  private static QueryState getNewQueryState(HiveConf conf) {
     return new QueryState.Builder().withGenerateNewQueryId(true).withHiveConf(conf).build();
   }
 }
