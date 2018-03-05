12d16586a047c184f646e6b0ab649fcef8b2c7eb
Zoltan Haindrich <kirk@rxd.hu>
2018-03-05 22:32:57 +0100

    add fixme

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/signature/OpTreeSignature.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/signature/OpTreeSignature.java
@@ -65,6 +65,7 @@ public class OpTreeSignature {
       return true;
     }
     OpTreeSignature o = (OpTreeSignature) obj;
+    // FIXME: this should be removed as soon as signatures are able to provide the same level of confidentiality as logicalEquals
     return logicalEqualsTree(op, o.op);
   }
 
