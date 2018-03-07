ccd21e566342aa4408abd1bdb74099c6fc97d472
Zoltan Haindrich <zhaindrich@hortonworks.com>
2018-03-07 13:39:14 +0100

    fix#3
    Change-Id: I718a081376d60e21a8fb0a69e56d09827e5276ce

--- ql/src/java/org/apache/hadoop/hive/ql/exec/DDLTask.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/DDLTask.java
@@ -4738,7 +4738,7 @@ public class DDLTask extends Task<DDLWork> implements Serializable {
     List<SQLDefaultConstraint> defaultConstraints = crtTbl.getDefaultConstraints();
     LOG.debug("creating table {} on {}",tbl.getFullyQualifiedName(),tbl.getDataLocation());
 
-    if (crtTbl.getLocation() != null) {
+    if (tbl.getTableType() != TableType.MANAGED_TABLE) {
       // should collect fs level stat info
       Hive.collectFsStats(tbl, null, conf);
     }
