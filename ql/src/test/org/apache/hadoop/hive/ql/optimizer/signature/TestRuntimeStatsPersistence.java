5d85431373aa18de602de896a52fe5dcf938b734
Zoltan Haindrich <kirk@rxd.hu>
2018-04-11 13:11:29 +0200

    ensure storage works

--- ql/src/test/org/apache/hadoop/hive/ql/optimizer/signature/TestRuntimeStatsPersistence.java
+++ ql/src/test/org/apache/hadoop/hive/ql/optimizer/signature/TestRuntimeStatsPersistence.java
@@ -19,16 +19,16 @@
 package org.apache.hadoop.hive.ql.optimizer.signature;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.apache.hadoop.hive.ql.CompilationOpContext;
 import org.apache.hadoop.hive.ql.exec.Operator;
 import org.apache.hadoop.hive.ql.exec.OperatorFactory;
 import org.apache.hadoop.hive.ql.metadata.Table;
+import org.apache.hadoop.hive.ql.optimizer.signature.SignatureUtils.SignaturePersister;
 import org.apache.hadoop.hive.ql.plan.ExprNodeColumnDesc;
 import org.apache.hadoop.hive.ql.plan.ExprNodeConstantDesc;
 import org.apache.hadoop.hive.ql.plan.ExprNodeDesc;
@@ -43,35 +43,11 @@ import org.junit.Test;
 
 import com.google.common.collect.Lists;
 
-public class TestOperatorSignature {
+public class TestRuntimeStatsPersistence {
 
   GenericUDF udf = new GenericUDFConcat();
 
   CompilationOpContext cCtx = new CompilationOpContext();
-
-  @Test
-  public void testFilterOpEquals() {
-    Operator<? extends OperatorDesc> op7 = getFilterOp(7);
-    Operator<? extends OperatorDesc> op8 = getFilterOp(8);
-    Operator<? extends OperatorDesc> op7b = getFilterOp(7);
-
-    checkEquals(op7, op7b);
-    checkNotEquals(op7, op8);
-  }
-
-  @Test
-  public void testTree1() {
-    Operator<?> tr37 = getFilTsOp(3, 7);
-    Operator<?> tr37a = getFilTsOp(3, 7);
-    Operator<?> tr17 = getFilTsOp(1, 7);
-    Operator<?> tr31 = getFilTsOp(3, 1);
-
-    checkEquals(tr37, tr37a);
-
-    checkTreeNotEquals(tr37, tr17);
-    checkTreeEquals(tr37, tr37a);
-    checkTreeNotEquals(tr37, tr31);
-  }
 
   private Operator<?> getFilTsOp(int i, int j) {
     Operator<TableScanDesc> ts = getTsOp(i);
@@ -87,58 +63,59 @@ public class TestOperatorSignature {
     child.getParentOperators().add(parent);
   }
 
+  OpTreeSignatureFactory signatureFactory = OpTreeSignatureFactory.newCache();
+
   @Test
-  public void testTableScand() {
-    Operator<TableScanDesc> t1 = getTsOp(3);
-    Operator<TableScanDesc> t1a = getTsOp(3);
-    Operator<TableScanDesc> t2 = getTsOp(4);
-
-    assertTrue(t1.logicalEquals(t1a));
-    checkEquals(t1, t1a);
-    checkNotEquals(t1, t2);
+  public void checkPersistingSigWorks() throws Exception {
+    OpSignature sig = OpSignature.of(getTsOp(3));
+    OpSignature sig2 = persistenceLoop(sig, OpSignature.class);
+    assertEquals(sig, sig2);
   }
 
-  public static void checkEquals(Operator<?> o1, Operator<?> o2) {
-    assertTrue(o1.logicalEquals(o2));
-    OpSignature s1 = OpSignature.of(o1);
-    OpSignature s2 = OpSignature.of(o2);
-
-    s1.proveEquals(s2);
-    assertTrue("sigCmp", s1.signatureCompare(s2));
-    assertEquals(s1.hashCode(), s2.hashCode());
-    assertEquals(s1, s2);
+  @Test
+  public void checkPersistingTreeSigWorks() throws Exception {
+    OpTreeSignature sig = signatureFactory.getSignature(getFilTsOp(3, 4));
+    OpTreeSignature sig2 = persistenceLoop(sig, OpTreeSignature.class);
+    assertEquals(sig, sig2);
   }
 
+  @Test
+  public void checkCanStoreAsGraph() throws Exception {
 
-  public static void checkNotEquals(Operator<? extends OperatorDesc> o1, Operator<? extends OperatorDesc> o2) {
-    assertFalse(o1.logicalEquals(o2));
-    OpSignature s1 = OpSignature.of(o1);
-    OpSignature s2 = OpSignature.of(o2);
+    Operator<?> ts = getTsOp(0);
+    Operator<?> fil1 = getFilterOp(1);
+    Operator<?> fil2 = getFilterOp(2);
+    Operator<?> fil3 = getFilterOp(3);
 
-    assertFalse(s1.signatureCompare(s2));
-    // this might be a little bit too much...but in most cases this should be true
-    assertNotEquals(s1.hashCode(), s2.hashCode());
-    assertNotEquals(s1, s2);
+    connectOperators(ts, fil1);
+    connectOperators(ts, fil2);
+    connectOperators(fil1, fil3);
+    connectOperators(fil2, fil3);
+
+    OpTreeSignature sig = signatureFactory.getSignature(fil3);
+    OpTreeSignature sig2 = persistenceLoop(sig, OpTreeSignature.class);
+
+    assertEquals(sig, sig2);
+
+    OpTreeSignature o0 = sig.getParentSig().get(0).getParentSig().get(0);
+    OpTreeSignature o1 = sig.getParentSig().get(1).getParentSig().get(0);
+    assertTrue("these have to be the same instance", o0 == o1);
+
+    OpTreeSignature p0 = sig2.getParentSig().get(0).getParentSig().get(0);
+    OpTreeSignature p1 = sig2.getParentSig().get(1).getParentSig().get(0);
+    assertTrue("these have to be the same instance", p0 == p1);
+
+    assertEquals(p0, p1);
+
   }
 
-  public static void checkTreeEquals(Operator<?> o1, Operator<?> o2) {
-    OpTreeSignature ts1 = OpTreeSignature.of(o1);
-    OpTreeSignature ts2 = OpTreeSignature.of(o2);
-
-    assertEquals(ts1.hashCode(), ts2.hashCode());
-    assertEquals(ts1, ts2);
+  private <T> T persistenceLoop(T sig, Class<T> clazz) throws IOException {
+    SignaturePersister sp = SignatureUtils.getSignaturePersister();
+    String stored = sp.encode(sig);
+    System.out.println(stored);
+    T sig2 = sp.decode(stored, clazz);
+    return sig2;
   }
-
-  public static void checkTreeNotEquals(Operator<? extends OperatorDesc> o1, Operator<? extends OperatorDesc> o2) {
-
-    OpTreeSignature ts1 = OpTreeSignature.of(o1);
-    OpTreeSignature ts2 = OpTreeSignature.of(o2);
-
-    assertNotEquals(ts1.hashCode(), ts2.hashCode());
-    ts1.equals(ts2);
-    assertNotEquals(ts1, ts2);
-  }
-
 
   private Operator<? extends OperatorDesc> getFilterOp(int constVal) {
     ExprNodeDesc pred = new ExprNodeConstantDesc(constVal);
@@ -151,15 +128,13 @@ public class TestOperatorSignature {
     Table tblMetadata = new Table("db", "table");
     // FIXME: I think this shouldn't be sensitive to the alias; but currently its included in logicalEquals...check that
     TableScanDesc desc = new TableScanDesc("alias"/*+ cCtx.nextOperatorId()*/, tblMetadata);
-      List<ExprNodeDesc> as =
+    List<ExprNodeDesc> as =
         Lists.newArrayList(new ExprNodeConstantDesc(TypeInfoFactory.intTypeInfo, Integer.valueOf(i)),
-          new ExprNodeColumnDesc(TypeInfoFactory.intTypeInfo, "c1", "aa", false));
+            new ExprNodeColumnDesc(TypeInfoFactory.intTypeInfo, "c1", "aa", false));
     ExprNodeGenericFuncDesc f1 = new ExprNodeGenericFuncDesc(TypeInfoFactory.intTypeInfo, udf, as);
     desc.setFilterExpr(f1);
     Operator<TableScanDesc> ts = OperatorFactory.get(cCtx, desc);
     return ts;
   }
-
-
 
 }
