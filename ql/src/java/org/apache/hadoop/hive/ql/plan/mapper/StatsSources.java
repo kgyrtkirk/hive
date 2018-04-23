60fce106f2cbbbf97e412e8d4506cf8698cb8627
Zoltan Haindrich <kirk@rxd.hu>
2018-04-23 09:00:09 +0200

    enum

--- ql/src/java/org/apache/hadoop/hive/ql/plan/mapper/StatsSources.java
+++ ql/src/java/org/apache/hadoop/hive/ql/plan/mapper/StatsSources.java
@@ -48,15 +48,15 @@ public class StatsSources {
   }
 
   public static StatsSource getStatsSource(HiveConf conf) {
-    StatsSourceMode mode = StatsSourceMode.valueOf(conf.getVar(ConfVars.HIVE_QUERY_REEXECUTION_STATS_PERSISTENCE));
+    String mode = conf.getVar(ConfVars.HIVE_QUERY_REEXECUTION_STATS_PERSISTENCE);
     int cacheSize = conf.getIntVar(ConfVars.HIVE_QUERY_REEXECUTION_STATS_CACHE_SIZE);
     int msBatchSize = conf.getIntVar(ConfVars.HIVE_RUNTIME_STATS_LOAD_BATCH_SIZE);
     switch (mode) {
-    case query:
+    case "query":
       return new MapBackedStatsSource();
-    case hiveserver:
+    case "hiveserver":
       return StatsSources.globalStatsSource(cacheSize);
-    case metastore:
+    case "metastore":
       return StatsSources.metastoreBackedStatsSource(StatsSources.globalStatsSource(cacheSize), msBatchSize);
     default:
       throw new RuntimeException("Unknown StatsSource setting: " + mode);
