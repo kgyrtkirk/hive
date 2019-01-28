5e7e7f8c9383cb4030f8d6893c62fdad1bee1857
Zoltan Haindrich <kirk@rxd.hu>
2019-01-28 14:16:47 +0100

    add sysou

--- ql/src/test/org/apache/hadoop/hive/ql/plan/mapping/TestReOptimization.java
+++ ql/src/test/org/apache/hadoop/hive/ql/plan/mapping/TestReOptimization.java
@@ -267,7 +267,9 @@ public class TestReOptimization {
       List<HiveFilter> hfs = g.getAll(HiveFilter.class);
       // FIXME: oss seems to contain duplicates
       //      List<HiveFilter> hf = g.getAll(HiveFilter.class);
-
+      if (hfs.size() != 0) {
+        System.out.println("asd");
+      }
       if (fos.size() > 0 && oss.size() > 0 && hfs.size() > 0) {
         fos.sort(TestCounterMapping.OPERATOR_ID_COMPARATOR.reversed());
 
